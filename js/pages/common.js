79177522de3fa24b569905a6b3bbd81197053576
Damir Hadiiev <damir.gadiev@gmail.com>
2019-12-28 21:45:29 +0200

    Add the card draft

---
+++ js/pages/common.js
@@ -0,0 +1,42 @@
+var scene = new THREE.Scene();
+scene.background = new THREE.Color('0x444444');
+var canvas = document.getElementById("card-coordinates");
+var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.width, 0.1, 1000);
+console.log(canvas.width);
+var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.gammaFactor = 2.2;
+renderer.gammaOutput = true;
+var geometry = new THREE.BoxGeometry(10, 10, 10);
+var material = new THREE.MeshPhongMaterial({ color: 'grey', side: THREE.FrontSide,
+shadowSide: THREE.FrontSide });
+var cube = new THREE.Mesh(geometry, material);
+cube.receiveShadow = true;
+cube.castShadow = true;
+cube.geometry.computeVertexNormals();
+scene.add(cube);
+
+// Spotlight shining from the side, casting a shadow
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(100, 1000, 100);
+
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 500;
+spotLight.shadow.camera.far = 4000;
+spotLight.shadow.camera.fov = 5;
+scene.add(spotLight);
+
+camera.position.z = 25;
+camera.position.y = -3;
+
+var animate = function () {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
+
+    renderer.render(scene, camera);
+};
+
+animate();
