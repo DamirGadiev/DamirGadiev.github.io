318181c0b06c6eefc613f86a6cb15f0da29e74e2
Damir Hadiiev <damir.gadiev@gmail.com>
2019-12-28 22:12:45 +0200

    Add simple structuring

--- js/pages/common.js
+++ js/pages/common.js
@@ -1,42 +1,66 @@
-var scene = new THREE.Scene();
-scene.background = new THREE.Color('#e7e7e7');
-var canvas = document.getElementById("card-coordinates");
-var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.width, 0.1, 1000);
-console.log(canvas.width);
-var renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.gammaFactor = 2.2;
-renderer.gammaOutput = true;
-var geometry = new THREE.BoxGeometry(10, 10, 10);
-var material = new THREE.MeshLambertMaterial({ color: 'grey', side: THREE.FrontSide,
-shadowSide: THREE.FrontSide });
-var cube = new THREE.Mesh(geometry, material);
-cube.receiveShadow = true;
-cube.castShadow = true;
-cube.geometry.computeVertexNormals();
-scene.add(cube);
+function CommonScene(id, label) {
+    // Add ID.
+    this.id = id;
+    // Add the label.
+    this.label = label;
+}
 
-// Spotlight shining from the side, casting a shadow
-var spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set(100, 1000, 100);
+CommonScene.prototype.initScene = function () {
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color('#e7e7e7');
+    if (document.getElementById(this.id)) {
+        var canvas = document.getElementById("card-coordinates");
+        var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.width, 0.1, 1000);
+        var renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+        renderer.gammaFactor = 2.2;
+        renderer.gammaOutput = true;
+        var geometry = new THREE.BoxGeometry(10, 10, 10);
+        var material = new THREE.MeshLambertMaterial({
+            color: 'grey', side: THREE.FrontSide,
+            shadowSide: THREE.FrontSide
+        });
+        var cube = new THREE.Mesh(geometry, material);
+        cube.receiveShadow = true;
+        cube.castShadow = true;
+        cube.geometry.computeVertexNormals();
+        scene.add(cube);
 
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 500;
-spotLight.shadow.camera.far = 4000;
-spotLight.shadow.camera.fov = 5;
-scene.add(spotLight);
+        // Spotlight shining from the side, casting a shadow
+        var spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(100, 1000, 100);
 
-camera.position.z = 25;
-camera.position.y = -3;
+        spotLight.castShadow = true;
+        spotLight.shadow.mapSize.width = 1024;
+        spotLight.shadow.mapSize.height = 1024;
+        spotLight.shadow.camera.near = 500;
+        spotLight.shadow.camera.far = 4000;
+        spotLight.shadow.camera.fov = 5;
+        scene.add(spotLight);
 
-var animate = function () {
-    requestAnimationFrame(animate);
+        camera.position.z = 25;
+        camera.position.y = -3;
 
-    cube.rotation.x += 0.001;
-    cube.rotation.y += 0.008;
+        var animate = function () {
+            requestAnimationFrame(animate);
 
-    renderer.render(scene, camera);
-};
+            cube.rotation.x += 0.001;
+            cube.rotation.y += 0.008;
 
-animate();
+            renderer.render(scene, camera);
+        };
+
+        animate();
+    }
+    else {
+        throw new Error("no canvas wrapper found...");
+    }
+
+}
+
+try {
+    var commonScene = new CommonScene("card-coordinates", "Test card");
+    commonScene.initScene();
+}
+catch (e) {
+    console.log(e);
+}
